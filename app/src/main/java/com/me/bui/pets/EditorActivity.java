738fdaedd379fbbf4ffe58068e1b6ee79f7fa3cf
mao.buidinh <mao.buidinh@hotmail.com>
2018-04-27 16:53:55 +0700

    - correct update/insert function in Edit Activity.

--- app/src/main/java/com/me/bui/pets/EditorActivity.java
+++ app/src/main/java/com/me/bui/pets/EditorActivity.java
@@ -16,9 +16,13 @@
 package com.me.bui.pets;
 
 import android.app.AlertDialog;
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
@@ -39,7 +43,7 @@ import com.me.bui.pets.data.PetContract.PetEntry;
 /**
  * Allows user to create a new pet or edit an existing one.
  */
-public class EditorActivity extends AppCompatActivity {
+public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
     private static final String TAG = EditorActivity.class.getName();
 
@@ -65,6 +69,8 @@ public class EditorActivity extends AppCompatActivity {
 
     private Uri mContentUri;
 
+    private static final int EXISTING_PET_LOADER = 0;
+
     private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -85,6 +91,7 @@ public class EditorActivity extends AppCompatActivity {
             invalidateOptionsMenu();
         } else {
             setTitle(getString(R.string.editor_activity_title_edit_pet));
+            getLoaderManager().initLoader(EXISTING_PET_LOADER, null, this);
         }
         // Find all relevant views that we will need to read user input from
         mNameEditText = (EditText) findViewById(R.id.edit_pet_name);
@@ -163,7 +170,7 @@ public class EditorActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Respond to a click on the "Save" menu option
             case R.id.action_save:
-                insertPet();
+                savePet();
                 finish();
                 return true;
             // Respond to a click on the "Delete" menu option
@@ -230,7 +237,7 @@ public class EditorActivity extends AppCompatActivity {
         alertDialog.show();
     }
 
-    private void insertPet () {
+    private void savePet() {
         String name = mNameEditText.getText().toString().trim();
         String breed = mBreedEditText.getText().toString().trim();
         int weight = Integer.parseInt(mWeightEditText.getText().toString().trim());
@@ -242,12 +249,27 @@ public class EditorActivity extends AppCompatActivity {
         values.put(PetEntry.COLUMN_PET_GENDER, mGender);
         values.put(PetEntry.COLUMN_PET_WEIGHT, weight);
 
-        Uri newUri = getContentResolver().insert(PetEntry.CONTENT_URI, values);
+        if (mContentUri == null) {
+            Uri newUri = getContentResolver().insert(PetEntry.CONTENT_URI, values);
 
-        if(newUri == null) {
-            Toast.makeText(this, R.string.editor_insert_pet_failed, Toast.LENGTH_LONG).show();
+            if(newUri == null) {
+                Toast.makeText(this, R.string.editor_insert_pet_failed, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, R.string.editor_insert_pet_successful, Toast.LENGTH_LONG).show();
+            }
         } else {
-            Toast.makeText(this, R.string.editor_insert_pet_successful, Toast.LENGTH_LONG).show();
+            int rowsAffected = getContentResolver().update(mContentUri, values, null, null);
+
+            // Show a toast message depending on whether or not the update was successful.
+            if (rowsAffected == 0) {
+                // If no rows were affected, then there was an error with the update.
+                Toast.makeText(this, getString(R.string.editor_update_pet_failed),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                // Otherwise, the update was successful and we can display a toast.
+                Toast.makeText(this, getString(R.string.editor_update_pet_successful),
+                        Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
@@ -282,7 +304,7 @@ public class EditorActivity extends AppCompatActivity {
      */
     private void deletePet() {
         if (mContentUri != null) {
-            int rowsDeleted = getContentResolver().delete(PetEntry.CONTENT_URI, null, null);
+            int rowsDeleted = getContentResolver().delete(mContentUri, null, null);
 
             // Show a toast message depending on whether or not the delete was successful.
             if (rowsDeleted == 0) {
@@ -297,5 +319,71 @@ public class EditorActivity extends AppCompatActivity {
         }
         // Close the activity
         finish();
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        String[] projection = {
+                PetEntry._ID,
+                PetEntry.COLUMN_PET_NAME,
+                PetEntry.COLUMN_PET_BREED,
+                PetEntry.COLUMN_PET_GENDER,
+                PetEntry.COLUMN_PET_WEIGHT };
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                mContentUri,         // Query the content URI for the current pet
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null); // Default sort order
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        if (cursor == null || cursor.getCount() < 1)
+            return;
+        if (cursor.moveToFirst()) {
+            // Find the columns of pet attributes that we're interested in
+            int nameColIdx = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME);
+            int breedColIdx = cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED);
+            int genderColIdx = cursor.getColumnIndex(PetEntry.COLUMN_PET_GENDER);
+            int weightColIdx = cursor.getColumnIndex(PetEntry.COLUMN_PET_WEIGHT);
+
+            // Extract out the value from the Cursor for the given column index
+            String name = cursor.getString(nameColIdx);
+            String breed = cursor.getString(breedColIdx);
+            int gender = cursor.getInt(genderColIdx);
+            int weight = cursor.getInt(weightColIdx);
+
+            // Update the views on the screen with the values from the database
+            mNameEditText.setText(name);
+            mBreedEditText.setText(breed);
+            mWeightEditText.setText(Integer.toString(weight));
+
+            // Gender is a dropdown spinner, so map the constant value from the database
+            // into one of the dropdown options (0 is Unknown, 1 is Male, 2 is Female).
+            // Then call setSelection() so that option is displayed on screen as the current selection.
+            switch (gender) {
+                case PetEntry.GENDER_MALE:
+                    mGenderSpinner.setSelection(1);
+                    break;
+                case PetEntry.GENDER_FEMALE:
+                    mGenderSpinner.setSelection(2);
+                    break;
+                default:
+                    mGenderSpinner.setSelection(0);
+                    break;
+            }
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mNameEditText.setText("");
+        mBreedEditText.setText("");
+        mWeightEditText.setText("");
+        mGenderSpinner.setSelection(0); // Select "Unknown" gender PetEntry.GENDER_UNKNOWN
     }
 }
